dc6c5b4a93dd57bb5ec0d8739ead3adcff65e6c9
Binesh Ellupurayil Balachandran <31845423+binesheb@users.noreply.github.com>
2026-09-13 19:00:16 +0530

    fix: export core configuration and exceptions

--- elan/core/__init__.py
+++ elan/core/__init__.py
@@ -1,8 +1,19 @@
 """Vendor-independent ELAN driver core."""
 
 from .capabilities import Capability
+from .config import DeviceCredentials, DriverConfig
 from .device import Device, DeviceStatus
 from .driver import Driver
+from .exceptions import (
+    DriverAuthenticationError,
+    DriverConnectionError,
+    DriverError,
+    UnsupportedCapabilityError,
+)
 from .registry import DriverRegistry
 
-__all__ = ["Capability", "Device", "DeviceStatus", "Driver", "DriverRegistry"]
+__all__ = [
+    "Capability", "Device", "DeviceStatus", "DeviceCredentials", "Driver",
+    "DriverConfig", "DriverError", "DriverAuthenticationError",
+    "DriverConnectionError", "UnsupportedCapabilityError", "DriverRegistry",
+]
